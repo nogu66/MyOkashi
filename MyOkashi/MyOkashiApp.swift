50abd0b5871a4b4cd1fafdecc8a4c8a55e478d17
nogu66 <ezochi66@gmail.com>
2023-05-28 01:42:01 +0900

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by YutaNoguchi on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
